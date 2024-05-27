432dd1f693c39a4a4cdddbbd6bbebfffd01d9f76
mimo626 <yessuk5499@gmail.com>
2024-05-27 13:47:29 +0900

    [create] menu.model 생성

---
+++ lib/domain/model/display/menu/menu.model.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'menu.model.freezed.dart';
+part 'menu.model.g.dart';
+
+@freezed
+class Menu with _$Menu {
+  const factory Menu({
+    required int tabId,
+    required String title
+  }) = _Menu;
+
+
+
+  factory Menu.fromJson(Map<String, dynamic> json) =>
+      _$MenuFromJson(json);
+}
