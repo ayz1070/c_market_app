5f9631440ebcfe9925efc7f18f57185b17cc042b
mimo626 <yessuk5499@gmail.com>
2024-05-28 09:39:21 +0900

    [create] domain의 usecase_menu

---
+++ lib/domain/usecase/base/display/menu/get_menus.usecase.dart
@@ -0,0 +1,25 @@
+import 'package:c_market_app/core/utils/constant.dart';
+import 'package:c_market_app/core/utils/error/error_response.dart';
+import 'package:c_market_app/domain/model/common/result.dart';
+import 'package:c_market_app/domain/repository/display.repository.dart';
+import 'package:c_market_app/domain/usecase/base/remote.usecase.dart';
+
+class GetMenusUsecase extends RemoteUsecase<DisplayRepository> {
+  final MallType mallType;
+
+  GetMenusUsecase(this.mallType);
+
+  @override
+  Future call(DisplayRepository repository) async {
+    final result = await repository.getMenuByMallType(mallType: mallType);
+
+    return (result.status == 'SUCCESS')
+        ? Result.Success(result.data ?? [])
+        : Result.failure(ErrorResponse(
+            status: result.status,
+            code: result.code,
+            message: result.message,
+          )
+    );
+  }
+}
