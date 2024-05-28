852fa0537d9fc2fb323cc003fa0b1110816e4d14
mimo626 <yessuk5499@gmail.com>
2024-05-28 10:15:46 +0900

    [create] service locator 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:c_market_app/core/utils/constant.dart';
 import 'package:c_market_app/data/data_source/mock/display/display_mock_api.dart';
 import 'package:c_market_app/data/repository_impl/display.repository_impl.dart';
+import 'package:c_market_app/domain/usecase/base/display/display.usecase.dart';
+import 'package:c_market_app/domain/usecase/base/display/menu/get_menus.usecase.dart';
 import 'package:c_market_app/presentation/routes/routes.dart';
+import 'package:c_market_app/service_locator.dart';
 import 'package:flutter/material.dart';
 
 void main() async{
   final data = await DisplayMockApi().getMenusByMallType('market');
   final data2 = await DisplayRepositoryImpl(DisplayMockApi()).getMenuByMallType(mallType: MallType.market);
-  print(data);
-  print(data2);
+  //print(data);
+  //print(data2);
+
+  final menus = await DisplayUsecase(DisplayRepositoryImpl(DisplayMockApi()))
+      .excute(usecase: GetMenusUsecase(mallType: MallType.market));
+  final menus2 = locator<DisplayUsecase>()
+  .excute(usecase: GetMenusUsecase(mallType: MallType.market));
+  print(menus2);
+  //setLocator();
   runApp(const MyApp());
 }
 
