dc1601892c9bd0f132060483104f08a88cb4ee22
mimo626 <yessuk5499@gmail.com>
2024-05-28 13:43:28 +0900

    [create] home의 menu_bloc 생성

---
+++ lib/presentation/pages/home/bloc/menu_bloc/menu_bloc.dart
@@ -0,0 +1,10 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../../../../core/utils/constant.dart';
+import '../../../../../core/utils/error/error_response.dart';
+
+part 'menu_event.dart';
+
+part 'menu_state.dart';
+
+part 'menu_bloc.freezed.dart';
