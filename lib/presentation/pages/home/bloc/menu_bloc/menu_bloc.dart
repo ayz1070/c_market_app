9ae39a90fca82dbaa192474b766708ff1c813550
mimo626 <yessuk5499@gmail.com>
2024-05-28 13:52:01 +0900

    [import] Menu bloc 오류 해결

--- lib/presentation/pages/home/bloc/menu_bloc/menu_bloc.dart
+++ lib/presentation/pages/home/bloc/menu_bloc/menu_bloc.dart
@@ -2,6 +2,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 import '../../../../../core/utils/constant.dart';
 import '../../../../../core/utils/error/error_response.dart';
+import '../../../../../domain/model/display/menu/menu.model.dart';
 
 part 'menu_event.dart';
 
