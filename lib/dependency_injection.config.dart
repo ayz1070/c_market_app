62d842b2cc24514bbcd8d45cc47998b681548637
mimo626 <yessuk5499@gmail.com>
2024-05-30 16:38:19 +0900

    [feat] DataSourceModule 생성, 구현

--- lib/dependency_injection.config.dart
+++ lib/dependency_injection.config.dart
@@ -8,14 +8,18 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:c_market_app/domain/usecase/base/display/display.usecase.dart'
+import 'package:c_market_app/data/data_source/data_source_module.dart' as _i9;
+import 'package:c_market_app/data/data_source/local_storage/display.dao.dart'
     as _i5;
+import 'package:c_market_app/data/data_source/remote/display.api.dart' as _i4;
+import 'package:c_market_app/domain/usecase/base/display/display.usecase.dart'
+    as _i7;
 import 'package:c_market_app/presentation/main/bloc/cart_bloc/cart_bloc.dart'
     as _i3;
 import 'package:c_market_app/presentation/main/bloc/payment_bloc/payment_bloc.dart'
-    as _i6;
+    as _i8;
 import 'package:c_market_app/presentation/pages/home/bloc/menu_bloc/menu_bloc.dart'
-    as _i4;
+    as _i6;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -30,9 +34,14 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
+    final dataSourceModule = _$DataSourceModule();
     gh.factory<_i3.CartBloc>(() => _i3.CartBloc());
-    gh.factory<_i4.MenuBloc>(() => _i4.MenuBloc(gh<_i5.DisplayUsecase>()));
-    gh.factory<_i6.PaymentBloc>(() => _i6.PaymentBloc());
+    gh.singleton<_i4.DisplayApi>(() => dataSourceModule.displayApi);
+    gh.singleton<_i5.DisplayDao>(() => dataSourceModule.displayDao);
+    gh.factory<_i6.MenuBloc>(() => _i6.MenuBloc(gh<_i7.DisplayUsecase>()));
+    gh.factory<_i8.PaymentBloc>(() => _i8.PaymentBloc());
     return this;
   }
 }
+
+class _$DataSourceModule extends _i9.DataSourceModule {}
