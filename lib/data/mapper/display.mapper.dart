5144eb7816e8c7ddf195c294b7a0240d2f7fdfcb
Kyung Soo Lee <blueeye0909@naver.com>
2024-05-30 10:25:53 +0900

    Feat: ViewModule(Domain) 리스트 구조 개발
    - Domain Layer 개발

--- lib/data/mapper/display.mapper.dart
+++ lib/data/mapper/display.mapper.dart
@@ -1,9 +1,21 @@
-import 'package:c_market_app/domain/model/display/menu/menu.model.dart';
+import 'package:c_market_app/domain/model/display/display.model.dart';
 
+import '../dto/display/display.dto.dart';
 import '../dto/display/menu/menu.dto.dart';
 
-extension MenuX on MenuDto{
-  Menu toModel(){
+extension MenuX on MenuDto {
+  Menu toModel() {
     return Menu(tabId: tabId ?? -1, title: title ?? '');
+  }
+}
+
+extension ViewModuleX on ViewModuleDto {
+  ViewModule toModel() {
+    return ViewModule(
+      type: type ?? '',
+      title: title ?? '',
+      subtitle: subtitle ?? '',
+      imageUrl: imageUrl ?? '',
+    );
   }
 }
