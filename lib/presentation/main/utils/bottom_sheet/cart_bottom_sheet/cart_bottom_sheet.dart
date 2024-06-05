99b20dfea4f5b4cabf933d57734f3e8df2697fc1
mimo626 <yessuk5499@gmail.com>
2024-06-05 17:54:14 +0900

    [feat] 장바구니 바텀 시트 컴포넌트 수정

--- lib/presentation/main/utils/bottom_sheet/cart_bottom_sheet/cart_bottom_sheet.dart
+++ lib/presentation/main/utils/bottom_sheet/cart_bottom_sheet/cart_bottom_sheet.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../../core/utils/extensions.dart';
+import '../../../../../core/utils/snack_bar/common_snack_bar.dart';
+import '../../../../pages/cart/bloc/cart_list_bloc/cart_list_bloc.dart';
 import '../../../bloc/cart_bloc/cart_bloc.dart';
 import 'widget/add_cart_btn.dart';
 import 'widget/cart_price_info.dart';
@@ -10,21 +14,29 @@ import 'widget/cart_product_info.dart';
 Future<bool?> cartBottomSheet(BuildContext context) {
   return showModalBottomSheet(context: context, builder: (_) {
     return SafeArea(
-        child: SingleChildScrollView(
-          child: BlocBuilder<CartBloc, CartState>(
-            builder: ( _, state){
-              return Column(
-                children: [
-                  CartProductInfo(productInfo: state.productInfo),
-                  const Divider(height: 1, thickness: 1,),
-                  CartPriceInfo(
-                    productInfo: state.productInfo,
-                    quantity: state.quantity,),
-                  AddCartBtn(totalPrice: state.totalPrice),
-                ],
-              );
+        child: BlocListener<CartListBloc, CartListState>(
+          listener: (_, state){
+            if(state.status.isError){
+              CommonSnackBar.errorSnackBar(context, error: state.error);
             }
-          ),
+            if (context.canPop()){
+              context.pop(!state.status.isError);
+            }
+          },
+          listenWhen: (pre, cur) =>
+          pre.status != cur.status && !cur.status.isLoading,
+          child: const SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CartProductInfo(),
+                  const Divider(height: 1, thickness: 1,),
+                  CartPriceInfo(),
+                  AddCartBtn(),
+                ],
+              ),
+            )
         ),
     );
   },
