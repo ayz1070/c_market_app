a7c1c61644f53f2f268d827783b0f0352e68ebac
mimo626 <yessuk5499@gmail.com>
2024-06-05 17:53:37 +0900

    [feat] 장바구니 담기 버튼 구현

--- lib/presentation/main/utils/bottom_sheet/cart_bottom_sheet/widget/add_cart_btn.dart
+++ lib/presentation/main/utils/bottom_sheet/cart_bottom_sheet/widget/add_cart_btn.dart
@@ -3,24 +3,28 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../../core/theme/custom/custom_font_weight.dart';
 import '../../../../../../core/utils/extensions.dart';
+import '../../../../../pages/cart/bloc/cart_list_bloc/cart_list_bloc.dart';
 import '../../../../bloc/cart_bloc/cart_bloc.dart';
 
 const double _btnHeight = 48;
 
 class AddCartBtn extends StatelessWidget {
-  const AddCartBtn({super.key, required this.totalPrice});
+  const AddCartBtn({Key? key}): super(key: key);
 
-  final int totalPrice;
 
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
-    final blocState = context.watch<CartBloc>().state;
+    final cartBloc = context.watch<CartBloc>().state;
 
     return GestureDetector(
-      //Todo 장바구니 담기 버튼
-      onTap: null,
+      onTap: () => context.read<CartListBloc>().add(
+        CartListAdded(
+          quantity: cartBloc.quantity,
+          productInfo: cartBloc.productInfo,
+        ),
+      ),
       child: Container(
         margin: const EdgeInsets.all(10),
         alignment: Alignment.center,
@@ -32,7 +36,7 @@ class AddCartBtn extends StatelessWidget {
         child: RichText(
           text: TextSpan(children: [
             TextSpan(
-              text: '${blocState.totalPrice.toWon()}',
+              text: '${cartBloc.totalPrice.toWon()}',
               style: textTheme.titleSmall?.semiBold
                   ?.copyWith(color: colorScheme.onPrimary),
             ),
