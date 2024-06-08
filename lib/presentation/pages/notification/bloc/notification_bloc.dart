d8ba7f3f392d4764d73012f3041b843196c41698
Kyung Soo Lee <blueeye0909@naver.com>
2024-06-09 00:27:04 +0900

    Feat: BLoC 사용
    - notification_bloc.dart 추가
    - notification_state.dart 추가
    - notification_event.dart 추가

---
+++ lib/presentation/pages/notification/bloc/notification_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../domain/model/display/notification/notification.model.dart';
+import '../../../../domain/usecase/display/notifications/fetch_notifications.dart';
+
+part 'notification_event.dart';
+
+part 'notification_state.dart';
+
+class NotificationBloc extends Bloc<NotificationEvent, NotificationState> {
+  final FetchNotifications fetchNotifications;
+
+  NotificationBloc(this.fetchNotifications) : super(NotificationInitial()) {
+    on<LoadNotificationsEvent>((event, emit) async {
+      try {
+        emit(NotificationLoading());
+        final notifications = await fetchNotifications();
+        emit(NotificationLoaded(notifications: notifications));
+      } catch (e) {
+        emit(NotificationError("Failed to fetch Notifications"));
+      }
+    });
+  }
+}
