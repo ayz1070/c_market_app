687046df8ead32823bc01de668906cf7a9ba421a
mimo626 <yessuk5499@gmail.com>
2024-06-11 11:49:02 +0900

    마이페이지 ui 수정

--- lib/presentation/pages/user/user_page.dart
+++ lib/presentation/pages/user/user_page.dart
@@ -1,10 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../routes/route_path.dart';
 import 'customer_service_tabbar_screen.dart';
 import 'delivery_address_screen.dart';
 import 'package:c_market_app/presentation/pages/user/set_alarm_screen.dart';
+
+import 'login_screen.dart';
 
 
 class UserPage extends StatelessWidget {
@@ -29,53 +33,58 @@ class UserPage extends StatelessWidget {
         builder: (context, snapshot) {
           return snapshot.hasData
               ? SingleChildScrollView(
-            padding: EdgeInsets.fromLTRB(20, 60, 20, 10),
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     Text(
                       '마이 페이지',
                       style: TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.bold),
+                          fontSize: 18, fontWeight: FontWeight.bold),
                     ),
                   ],
                 ),
-                Row(
-                  children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                            radius: 30,
-                            backgroundImage: NetworkImage(
-                              //'https://flexible.img.hani.co.kr/flexible/normal/960/960/imgdb/resize/2019/0121/00501111_20190121.JPG')),
-                                (snapshot.data
-                                as Map)['user_image_url'])),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text((snapshot.data as Map)['user_nickname'],
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold)),
-                      ],
-                    ),
-                    Spacer(),
-                    IconButton(
-                      icon: Icon(
-                        Icons.keyboard_arrow_right,
-                        size: 30,
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Row(
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                              radius: 30,
+                              backgroundImage: NetworkImage(
+                                //'https://flexible.img.hani.co.kr/flexible/normal/960/960/imgdb/resize/2019/0121/00501111_20190121.JPG')),
+                                  (snapshot.data
+                                  as Map)['user_image_url'])),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text((snapshot.data as Map)['user_nickname'],
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold)),
+                        ],
                       ),
-                      onPressed: null,
-                    ),
-                  ],
+                      Spacer(),
+                      IconButton(
+                        icon: Icon(
+                          Icons.keyboard_arrow_right,
+                          size: 30,
+                        ),
+                        onPressed: null,
+                      ),
+                    ],
+                  ),
                 ),
-                SizedBox(height: 30),
+                SizedBox(height: 20),
+                Divider(height: 1, thickness: 1,),
+                SizedBox(height: 20),
                 Text('구매',
                     style: TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold)),
+                        fontSize: 16, fontWeight: FontWeight.bold)),
                 TextButton(
                   onPressed: () {},
                   style: TextButton.styleFrom(
@@ -97,9 +106,11 @@ class UserPage extends StatelessWidget {
                         alignment: Alignment.centerLeft),
                     child: Text('배송지 관리')),
                 SizedBox(height: 10),
+                Divider(height: 1, thickness: 1,),
+                SizedBox(height: 20),
                 Text('고객센터',
                     style: TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold)),
+                        fontSize: 16, fontWeight: FontWeight.bold)),
                 TextButton(
                     onPressed: () {
                       Navigator.of(context).push(MaterialPageRoute(
@@ -128,9 +139,11 @@ class UserPage extends StatelessWidget {
                         alignment: Alignment.centerLeft),
                     child: Text('1:1문의')),
                 SizedBox(height: 10),
+                Divider(height: 1, thickness: 1,),
+                SizedBox(height: 20),
                 Text('설정',
                     style: TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold)),
+                        fontSize: 16, fontWeight: FontWeight.bold)),
                 TextButton(
                     onPressed: () {
                       Navigator.of(context).push(MaterialPageRoute(
@@ -142,11 +155,7 @@ class UserPage extends StatelessWidget {
                 TextButton(
                     onPressed: () {
                       FirebaseAuth.instance.signOut();
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                          builder: (ctx) => LoginScreen(),
-                        ),
-                      );
+                      context.push(RoutePath.login);
                     },
                     style: TextButton.styleFrom(
                         alignment: Alignment.centerLeft),
